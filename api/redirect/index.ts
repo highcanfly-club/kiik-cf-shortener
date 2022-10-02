2574caa39266d53e1942920f8cb76390c6914598
Ronan LE MEILLAT <eltorio@sctg.eu.org>
2022-10-02 17:29:45 +0200

    force creating dev

--- api/redirect/index.ts
+++ api/redirect/index.ts
@@ -4,7 +4,7 @@ import { getLongUrl } from "../common/cosmosdb";
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
     const _to = (req.query.to || (req.body && req.body.to));
-    const to = _to ? _to : await getLongUrl('np326')
+    const to = _to ? _to : await getLongUrl('Zp2MT')
     context.res = {
         status:302,
         headers: { "location": to }
