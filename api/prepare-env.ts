ba40c931f79ba0311512077f863bc1f9d6c3ce6b
Ronan LE MEILLAT <rtkgps@outlook.com>
2022-10-06 09:04:19 +0200

    rework prepare-env

--- api/prepare-env.ts
+++ api/prepare-env.ts
@@ -1,69 +1,79 @@
 /*generate auth0-conf.json*/
-import fs from "fs"
-import https from "https"
+import fs from "fs";
+import https from "https";
 const auth0Conf = {
-    "domain": process.env.AUTH0_DOMAIN,
-    "client_id": process.env.AUTH0_CLIENT_ID,
-    "scope": 'openid email profile user_metadata app_metadata picture',
-    "useRefreshTokens": true,
-    "cacheLocation": "localstorage",
-    "audience": "https://kiik.api"
-  };
-  fs.writeFile('./common/config/auth0-conf.json',
+  domain: process.env.AUTH0_DOMAIN,
+  client_id: process.env.AUTH0_CLIENT_ID,
+  scope: "openid email profile user_metadata app_metadata picture",
+  useRefreshTokens: true,
+  cacheLocation: "localstorage",
+  audience: "https://kiik.api",
+};
+fs.writeFile(
+  "./common/config/auth0-conf.json",
   JSON.stringify(auth0Conf),
-  'utf8', function (err) {
+  "utf8",
+  function (err) {
     if (err) return console.log(err);
   }
 );
 
-  export interface Auth0JWKS {
-    alg: string;
-    kty: string;
-    use: string;
-    n: string;
-    e: string;
-    kid: string;
-    x5t: string;
-    x5c: string[];
-    domain: string;
-    namespace: string;
-  }
-  
-  async function getJwks() {
-    console.log(
-      `retrieve https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
-    );
-    const url = `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`;
-    return new Promise<Auth0JWKS>((resolve, reject) => {
-      https
-        .get(url, (res) => {
-          let data = "" as string;
-          res.on("data", (chunk) => {
-            data += chunk;
-          });
-          res.on("end", () => {
-            const structuredData = JSON.parse(data) as Auth0JWKS;
-            structuredData.domain = process.env.AUTH0_DOMAIN;
-            structuredData.namespace = process.env.AUTH0_CUSTOM_NAMESPACE;
-            resolve(structuredData);
-          });
-        })
-        .on("error", (err) => {
-          console.log(err.message);
-          reject(err);
+export interface KeyJWKS {
+  alg: string;
+  kty: string;
+  use: string;
+  n: string;
+  e: string;
+  kid: string;
+  x5t: string;
+  x5c: string[];
+}
+export interface JWKS {
+  keys:KeyJWKS[];
+}
+export interface Auth0JWKS extends JWKS {
+  domain: string;
+  namespace: string;
+}
+
+async function getJwks() {
+  console.log(
+    `retrieve https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+  );
+  const url = `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`;
+  return new Promise<Auth0JWKS>((resolve, reject) => {
+    https
+      .get(url, (res) => {
+        let data = "" as string;
+        res.on("data", (chunk) => {
+          data += chunk;
         });
-    });
-  }
-  
-  (async () => {
-    const jwks = await getJwks();
-    console.log(JSON.stringify(jwks))
-    fs.writeFile(
-      "./common/config/jwks.json",
-      JSON.stringify(jwks),
-      "utf8",
-      function (err) {
-        if (err) return console.log(err);
-      }
-    );
-  })();
+        res.on("end", () => {
+          let structuredData = {} as Auth0JWKS;
+          structuredData = {
+            ...(JSON.parse(data) as JWKS),
+            domain: process.env.AUTH0_DOMAIN,
+            namespace: process.env.AUTH0_CUSTOM_NAMESPACE,
+          };
+          resolve(structuredData);
+        });
+      })
+      .on("error", (err) => {
+        console.log(err.message);
+        reject(err);
+      });
+  });
+}
+
+(async () => {
+  const jwks = await getJwks();
+  console.log(JSON.stringify(jwks));
+  fs.writeFile(
+    "./common/config/jwks.json",
+    JSON.stringify(jwks),
+    "utf8",
+    function (err) {
+      if (err) return console.log(err);
+    }
+  );
+})();
