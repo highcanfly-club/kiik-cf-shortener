8d585b2a8139b607598625f94c286cc72bdccfdf
Ronan LE MEILLAT <eltorio@sctg.eu.org>
2022-10-06 18:48:27 +0200

    add spanish

--- src/main.ts
+++ src/main.ts
@@ -12,14 +12,14 @@ import { createWebHistory, createRouter, RouteRecordRaw } from "vue-router";
 import type { Auth0Instance, RedirectCallback } from "@/auth0";
 import { initAuth0 } from "@/auth0";
 import auth0conf from "@/config/auth0-conf.json";
-import { createI18n } from "vue-i18n"
-import { createPinia } from 'pinia'
+import { createI18n } from "vue-i18n";
+import { createPinia } from "pinia";
 import "@/index.scss";
 
-import enUS from '@/locales/en-US.json'
+import enUS from "@/locales/en-US.json";
 
-type MessageSchema = typeof enUS
-type Messages = {"fr-FR"?:MessageSchema;"en-US"?:MessageSchema;}
+type MessageSchema = typeof enUS;
+type Messages = { "fr-FR"?: MessageSchema; "en-US"?: MessageSchema };
 
 declare module "@vue/runtime-core" {
   interface ComponentCustomProperties {
@@ -34,18 +34,18 @@ const routes = [
     component: () => import("@/views/IndexPage.vue"),
     name: "index",
   },
-
 ] as RouteRecordRaw[];
 
-const i18n = createI18n<[MessageSchema | string], 'fr-FR' | 'en-US'>({
-  locale: 'en-US',
+const i18n = createI18n<[MessageSchema | string], "fr-FR" | "en-US" | "es-ES">({
+  locale: "en-US",
   legacy: false,
-  fallbackLocale: 'fr-FR',
+  fallbackLocale: "fr-FR",
   messages: {
-      'fr-FR': '', //will be lazily loaded in HeaderMain/changeLang(locale)
-      'en-US': enUS,
-  }
-}) 
+    "fr-FR": "", //will be lazily loaded in HeaderMain/changeLang(locale)
+    "en-US": enUS,
+    "es-ES": "",
+  },
+});
 
 const router = createRouter({
   scrollBehavior(to) {
@@ -59,25 +59,21 @@ const router = createRouter({
   routes,
 });
 
-const pinia = createPinia()
+const pinia = createPinia();
 const app = createApp(App);
 
-const useImage = ((url: string) => {
+const useImage = (url: string) => {
   return new URL(`/src/${url}`, import.meta.url).href;
-});
+};
 
-app.use(pinia).use(i18n).use(router)
+app.use(pinia).use(i18n).use(router);
 app.mount("#app");
 
 const REDIRECT_CALLBACK: RedirectCallback = () =>
-  window.history.replaceState(
-    {},
-    document.title,
-    `${window.location.origin}/`
-  );
+  window.history.replaceState({}, document.title, `${window.location.origin}/`);
 
-  app.config.globalProperties.$require = useImage
-  app.config.globalProperties.$auth0 = initAuth0({
+app.config.globalProperties.$require = useImage;
+app.config.globalProperties.$auth0 = initAuth0({
   onRedirectCallback: REDIRECT_CALLBACK,
   redirectUri: `${window.location.origin}/`,
   ...auth0conf,
