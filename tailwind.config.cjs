26d280d0ab0da867ea7c6881187d48c097e3d41f
Ronan L <aeltorio@universidadatlantico.org>
2026-05-22 17:40:17 +0200

    update step 7

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,7 +10,27 @@ This website use:
 
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  safelist: [{ pattern: /^font-neutra-/ }],
   //safelist: (process.env.NODE_ENV !== 'development') ? [] : [{ pattern: /.*/ }], // not usefull with vite
   darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        "neutra-bold": ["NeutrafaceText-Bold"],
+        "neutra-boldsc": ["NeutrafaceText-BoldSC"],
+        "neutra-boldalt": ["NeutrafaceText-BoldAlt"],
+        "neutra-book": ["NeutrafaceText-Book"],
+        "neutra-booknoambiguity": ["NeutrafaceText-BookNoAmbiguity"],
+        "neutra-booksc": ["NeutrafaceText-BookSC"],
+        "neutra-bookitalic": ["NeutrafaceText-BookItalic"],
+        "neutra-demi": ["NeutrafaceText-Demi"],
+        "neutra-demisc": ["NeutrafaceText-DemiSC"],
+        "neutra-demiitalic": ["NeutrafaceText-DemiItalic"],
+        "neutra-light": ["NeutrafaceText-Light"],
+        "neutra-lightsc": ["NeutrafaceText-LightSC"],
+        "neutra-lightitalic": ["NeutrafaceText-LightItalic"],
+      },
+    },
+  },
   plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 };
